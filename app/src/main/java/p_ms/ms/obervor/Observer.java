8d99df2086eff39fdb871bde71c594dce82e8078
Youssef Laarossi <laarossi.youssef@gmail.com>
2022-06-14 15:19:47 -0400

    Delete app/src/main/java/p_ms/ms directory

--- app/src/main/java/p_ms/ms/obervor/Observer.java
+++
@@ -1,7 +0,0 @@
-package p_ms.ms.obervor;
-
-public abstract class Observer {
-
-    public abstract void update(Integer t);
-
-}
--- app/src/main/java/p_ms/ms/obervor/Subject.java
+++
@@ -1,8 +0,0 @@
-package p_ms.ms.obervor;
-
-public interface Subject {
-
-    void addObserver(Observer observer);
-    void notifyObservers(int score);
-
-}
